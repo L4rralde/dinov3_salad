2e312bae1027ff107d32f065708b5ca2437877a5
l4rralde <ealarralde@gmail.com>
2025-09-06 17:44:17 -0600

    Bug fix. When num of val datasets is 1, dataloader_idx is not passed by torch.lightning

--- vpr_model.py
+++ vpr_model.py
@@ -192,6 +192,8 @@ class VPRModel(pl.LightningModule):
     # For validation, we will also iterate step by step over the validation set
     # this is the way Pytorch Lghtning is made. All about modularity, folks.
     def validation_step(self, batch, batch_idx, dataloader_idx=None):
+        if dataloader_idx is None and len(self.trainer.datamodule.val_datasets) == 1:
+            dataloader_idx = 0
         places, _ = batch
         descriptors = self(places)
         self.val_outputs[dataloader_idx].append(descriptors.detach().cpu())
