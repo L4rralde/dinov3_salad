c9909699a9b543e6fc2e3dc6d36a11ccb49f8f3e
l4rralde <ealarralde@gmail.com>
2025-09-17 09:18:48 -0600

    optionally use rope_sincos

--- models/backbones/dinov3/dinov3.py
+++ models/backbones/dinov3/dinov3.py
@@ -5,31 +5,14 @@ from .dinov3_repo import DINOV3_REPO_PATH
 from .urls import dinov3_vitb16
 
 
-
-DINOV3_ARCHS = {
-    'dinov3_vitb16': 768
-}   #TODO
-
-# where MODEL_NAME can be one of:
-# - dinov3_vits16
-# - dinov3_vits16plus
-# - dinov3_vitb16
-# - dinov3_vitl16
-# - dinov3_vith16plus
-# - dinov3_vit7b16
-# - dinov3_convnext_tiny
-# - dinov3_convnext_small
-# - dinov3_convnext_base
-# - dinov3_convnext_large
-
-
 class DINOv3(nn.Module):
     def __init__(
             self,
             model_name: str,
             num_trainable_blocks: int = 2,
-            norm_layer: bool = False, #True
-            return_token: bool = False, #True
+            norm_layer: bool = False,
+            return_token: bool = False,
+            rope_sincos: bool = True,
     ) -> None:
         super().__init__()
         self.model_name = model_name
@@ -43,6 +26,7 @@ class DINOv3(nn.Module):
         self.num_trainable_blocks = num_trainable_blocks
         self.norm_layer = norm_layer
         self.return_token = return_token
+        self.rope_sincos = rope_sincos
 
         if self.num_trainable_blocks > 0:
             self.frozen_blocks = self.model.blocks[:-self.num_trainable_blocks]
@@ -66,7 +50,10 @@ class DINOv3(nn.Module):
         B, _, h, w = x.shape
         x, (H, W) = self.model.prepare_tokens_with_masks(x)
 
-        rope_sincos = self.model.rope_embed(H=H, W=W)
+        if self.rope_sincos:
+            rope_sincos = self.model.rope_embed(H=H, W=W)
+        else:
+            rope_sincos = None
 
         # First blocks are frozen
         with torch.no_grad():
@@ -81,7 +68,7 @@ class DINOv3(nn.Module):
             x = self.model.norm(x)
 
         class_token = x[:, 0]
-        extra_token = x[:, 1: self.model.n_storage_tokens + 1] #register tokens? See appendix. Probably it adds nothing for inference
+        register_token = x[:, 1: self.model.n_storage_tokens + 1] #Probably it adds nothing for inference
 
         features = x[:, self.model.n_storage_tokens + 1 :]
 
