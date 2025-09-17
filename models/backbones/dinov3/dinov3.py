ac3fb2498e87b62775bacb6990f14860678a02c0
l4rralde <ealarralde@gmail.com>
2025-09-17 16:40:52 -0600

    Adding arg for linear probing

--- models/backbones/dinov3/dinov3.py
+++ models/backbones/dinov3/dinov3.py
@@ -13,8 +13,10 @@ class DINOv3(nn.Module):
             norm_layer: bool = False,
             return_token: bool = False,
             rope_sincos: bool = True,
+            probing_from_layer: int = None
     ) -> None:
         super().__init__()
+
         self.model_name = model_name
         self.model = torch.hub.load(
             DINOV3_REPO_PATH,
@@ -24,15 +26,32 @@ class DINOv3(nn.Module):
         )
         self.num_channels = self.model.num_features
         self.num_trainable_blocks = num_trainable_blocks
+        self.num_blocks = len(self.model.blocks)
         self.norm_layer = norm_layer
         self.return_token = return_token
         self.rope_sincos = rope_sincos
+        self.linear_probing = probing_from_layer is not None
+
+        #Validate inputs
+        if self.linear_probing:
+            assert num_trainable_blocks <= (probing_from_layer + 1), \
+                'num_trainable_blocks exceeds available layers for probing'
+            assert 0 <= probing_from_layer < self.num_blocks, \
+                f'probing_from_layer must be between 0 and {self.num_blocks - 1}'
+
+        assert 0 <= num_trainable_blocks <= self.num_blocks, \
+            f'num_trainable_blocks must be between 0 and total blocks: {self.num_blocks}'
+
+        if not self.linear_probing:
+            valid_blocks = self.model.blocks
+        else:
+            valid_blocks = self.model.blocks[:probing_from_layer + 1]
 
         if self.num_trainable_blocks > 0:
-            self.frozen_blocks = self.model.blocks[:-self.num_trainable_blocks]
-            self.trainable_blocks = self.model.blocks[-self.num_trainable_blocks:]
+            self.frozen_blocks = valid_blocks[:-self.num_trainable_blocks]
+            self.trainable_blocks = valid_blocks[-self.num_trainable_blocks:]
         else:
-            self.frozen_blocks = self.model.blocks
+            self.frozen_blocks = valid_blocks
             self.trainable_blocks = []
         
         self.freeze_blocks()
@@ -42,7 +61,7 @@ class DINOv3(nn.Module):
             for param in blk.parameters():
                 param.requires_grad = False
         
-        if self.num_trainable_blocks == 0:
+        if self.num_trainable_blocks == 0 and not self.linear_probing:
             for param in self.model.norm.parameters():
                 param.requires_grad = False
 
@@ -65,7 +84,11 @@ class DINOv3(nn.Module):
             x = blk(x, rope_sincos)
 
         if self.norm_layer:
-            x = self.model.norm(x)
+            if self.num_trainable_blocks == 0 and not self.linear_probing:
+                with torch.no_grad():
+                    x = self.model.norm(x)
+            else:
+                x = self.model.norm(x)
 
         class_token = x[:, 0]
         register_token = x[:, 1: self.model.n_storage_tokens + 1] #Probably it adds nothing for inference
